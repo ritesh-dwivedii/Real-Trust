3a83b043a03bf54d35b0b6eb306009d597e3cd4a
ritesh-dwivedii <riteshs2902@gmail.com>
2025-07-01 07:58:17 +0530

    deployed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+//const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://real-trust-2dts.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
